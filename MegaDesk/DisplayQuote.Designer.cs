608685e0bc75ed73fcb1518e6cd6f61b6b007540
TAP <tapetersen966@gmail.com>
2019-10-10 10:27:21 -0700

    Current issues: there is a value not binding to its variable in AddQuote and the close button in display quote does not go to the main menu

--- MegaDesk/DisplayQuote.Designer.cs
+++ MegaDesk/DisplayQuote.Designer.cs
@@ -28,19 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ReadFile = new System.Windows.Forms.TextBox();
+            this.SeeQuote = new System.Windows.Forms.Button();
+            this.close = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // ReadFile
+            // 
+            this.ReadFile.Location = new System.Drawing.Point(53, 196);
+            this.ReadFile.Name = "ReadFile";
+            this.ReadFile.ReadOnly = true;
+            this.ReadFile.Size = new System.Drawing.Size(449, 20);
+            this.ReadFile.TabIndex = 6;
+            this.ReadFile.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // SeeQuote
+            // 
+            this.SeeQuote.Location = new System.Drawing.Point(427, 222);
+            this.SeeQuote.Name = "SeeQuote";
+            this.SeeQuote.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.SeeQuote.Size = new System.Drawing.Size(75, 23);
+            this.SeeQuote.TabIndex = 7;
+            this.SeeQuote.Text = "See Quote";
+            this.SeeQuote.UseVisualStyleBackColor = true;
+            this.SeeQuote.Click += new System.EventHandler(this.SeeQuote_Click);
+            // 
+            // close
+            // 
+            this.close.Location = new System.Drawing.Point(53, 222);
+            this.close.Name = "close";
+            this.close.Size = new System.Drawing.Size(75, 23);
+            this.close.TabIndex = 8;
+            this.close.Text = "Close";
+            this.close.UseVisualStyleBackColor = true;
+            this.close.Click += new System.EventHandler(this.close_Click);
             // 
             // DisplayQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(554, 413);
+            this.Controls.Add(this.close);
+            this.Controls.Add(this.SeeQuote);
+            this.Controls.Add(this.ReadFile);
             this.Name = "DisplayQuote";
             this.Text = "Display Quote";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox ReadFile;
+        private System.Windows.Forms.Button SeeQuote;
+        private System.Windows.Forms.Button close;
     }
 }
--- MegaDesk/DisplayQuote.cs
+++ MegaDesk/DisplayQuote.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace MegaDesk
 {
@@ -15,6 +16,25 @@ namespace MegaDesk
         public DisplayQuote()
         {
             InitializeComponent();
+        }
+
+        private void SeeQuote_Click(object sender, EventArgs e)
+        {
+            StreamReader reader = new StreamReader("newQuote.txt");
+            while (reader.EndOfStream == false)
+            {
+                string line = reader.ReadLine();
+                ReadFile.Text += line + " ";
+                Console.WriteLine(line);
+            }
+            reader.Close();
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            MainMenu viewMainMenu = (MainMenu)Tag;
+            viewMainMenu.Show();
+            Close();
         }
     }
 }
